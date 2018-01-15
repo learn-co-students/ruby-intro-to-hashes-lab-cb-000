e7b47419bea8653db9b51c2c60dc39ca6b604952
David Hopkins <dlhopkins2@gmail.com>
2018-01-15 21:26:17 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,16 +29,16 @@ def monopoly_with_third_tier
 		railroads: {
 			pieces: 4,
 			names: {
-				reading_railroad: {},
-				pennsylvania_railroad: {},
-				b_and_o_railroad: {},
-				shortline: {},
+				:reading_railroad => {},
+				:pennsylvania_railroad => {},
+				:b_and_o_railroad => {},
+				:shortline => {},
 			},
 			rent_in_dollars: {
-				one_piece_owned: 25,
-				two_pieces_owned: 50,
-				three_pieces_owned: 100,
-				four_pieces_owned: 200,
+				:one_piece_owned => 25,
+				:two_pieces_owned => 50,
+				:three_pieces_owned => 100,
+				:four_pieces_owned => 200,
 			}
 		}
 	}
@@ -49,24 +49,24 @@ def monopoly_with_fourth_tier
 		railroads: {
 			pieces: 4,
 			names: {
-				reading_railroad: {
+				:reading_railroad => {
 					"mortgage_value" => "$100",
 				},
-				pennsylvania_railroad: {
+				:pennsylvania_railroad => {
 					"mortgage_value" => "$200",
 				},
-				b_and_o_railroad: {
+				:b_and_o_railroad => {
 					"mortgage_value" => "$400",
 				},
-				shortline: {
+				:shortline => {
 					"mortgage_value" => "$800",
 				}
 			},
 			rent_in_dollars: {
-				one_piece_owned: 25,
-				two_pieces_owned: 50,
-				three_pieces_owned: 100,
-				four_pieces_owned: 200,
+				:one_piece_owned => 25,
+				:two_pieces_owned => 50,
+				:three_pieces_owned => 100,
+				:four_pieces_owned => 200,
 			}
 		}
 	}
